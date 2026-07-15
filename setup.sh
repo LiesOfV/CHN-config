dd99b8455f1fcdce649cfd549d18977774f174f0
LiesOfV <anti-spam@sudo.net.za>
2026-07-15 17:56:35 +0000

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,42 @@
+# 1 - Audio stack
+
+sudo pacman -S --needed --noconfirm \
+  pipewire \
+  pipewire-pulse \
+  pipewire-alsa \
+  pipewire-jack \
+  wireplumber \
+  alsa-utils \
+  lib32-pipewire \
+  lib32-pipewire-jack
+
+
+  # 2 - Drivers and gaming stuff 
+
+  sudo pacman -S --needed --noconfirm \
+  mesa \
+  lib32-mesa \
+  vulkan-radeon \
+  lib32-vulkan-radeon \
+  vulkan-icd-loader \
+  lib32-vulkan-icd-loader \
+  libva \
+  lib32-libva \
+  libva-mesa-driver \
+  lib32-libva-mesa-driver \
+  gamemode \
+  lib32-gamemode \
+  alsa-plugins \
+  lib32-alsa-plugins \
+  openal \
+  lib32-openal \
+  mpg123 \
+  lib32-mpg123 \
+  lib32-gtk3 \
+  libxslt \
+  libjpeg-turbo \
+  lib32-libjpeg-turbo \
+  giflib \
+  lib32-giflib \
+  ttf-liberation
+  
