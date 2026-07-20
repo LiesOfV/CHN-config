cc9395947aa57b11cd33ab9519d0d6c4ccf1faf7
LiesOfV <anti-spam@sudo.net.za>
2026-07-20 11:05:30 +0000

    Create config.fish

---
+++ ~/.config/fish/config.fish
@@ -0,0 +1,2 @@
+alias s='pactl set-sink-port alsa_output.pci-0000_11_00.6.analog-stereo analog-output-lineout'
+alias h='pactl set-sink-port alsa_output.pci-0000_11_00.6.analog-stereo analog-output-headphones' 
